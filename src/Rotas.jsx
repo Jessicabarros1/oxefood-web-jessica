8839da87ea83f9edc10f325a0304c2401125d5a7
aluno <aluno@ifpe.com>
2023-04-27 20:41:31 -0300

    atualizacao menu

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
 
-import FormCliente from './views/cliente/FormCliente';
 import FormEntregador from './views/Entregador/FormEntregador';
+import ListEntregador from './views/Entregador/ListEntregador';
+import FormCliente from './views/cliente/FormCliente';
+import ListCliente from './views/cliente/ListCliente';
 import Home from './views/home/Home';
 import FormProduto from './views/produto/FormProduto';
 
@@ -11,8 +13,11 @@ function Rotas() {
         <>
             <Routes>
                 <Route path="/" element={ <Home/> } />
+                <Route path="list-cliente" element={ <ListCliente/> } />
                 <Route path="form-cliente" element={ <FormCliente/> } />
+               
                 <Route path="form-produto" element={ <FormProduto/> } />
+                <Route path="list-Entregador" element={ <ListEntregador/> } />
                 <Route path="form-entregador" element={ <FormEntregador/> } />
             </Routes>
         </>
