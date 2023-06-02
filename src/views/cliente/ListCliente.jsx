da89f2f9993845305a98d07c0a38445ad583e6a4
aluno <aluno@ifpe.com>
2023-06-02 20:12:00 -0300

    atualizacao

--- src/views/cliente/ListCliente.jsx
+++ src/views/cliente/ListCliente.jsx
@@ -3,121 +3,129 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button, Container, Divider, Icon, Table } from 'semantic-ui-react';
 
-class ListCliente extends React.Component{
+class ListCliente extends React.Component {
 
-   state = {
+    state = {
 
-       listaClientes: []
-      
-   }
+        listaClientes: []
 
-   componentDidMount = () => {
-      
-       this.carregarLista();
-      
-   }
-   carregarLista = () => {
+    }
 
-    axios.get("http://localhost:8082/api/cliente")
-    .then((response) => {
-       
-        this.setState({
-            listaClientes: response.data
-        })
-    })
+    componentDidMount = () => {
 
-};
+        this.carregarLista();
 
-formatarData = (dataParam) => {
+    }
+    carregarLista = () => {
 
-     if (dataParam == null || dataParam == '') {
-         return ''
-     }
-     
-     let dia = dataParam.substr(8,2);
-     let mes = dataParam.substr(5,2);
-     let ano = dataParam.substr(0,4);
-     let dataFormatada = dia + '/' + mes + '/' + ano;
+        axios.get("http://localhost:8082/api/cliente")
+            .then((response) => {
 
-     return dataFormatada
- };
- render(){
-    return(
-        <div>
+                this.setState({
+                    listaClientes: response.data
+                })
+            })
 
-            <div style={{marginTop: '3%'}}>
+    };
 
-                <Container textAlign='justified' >
+    formatarData = (dataParam) => {
 
-                    <h2> Cliente </h2>
+        if (dataParam == null || dataParam == '') {
+            return ''
+        }
 
-                    <Divider />
+        let dia = dataParam.substr(8, 2);
+        let mes = dataParam.substr(5, 2);
+        let ano = dataParam.substr(0, 4);
+        let dataFormatada = dia + '/' + mes + '/' + ano;
 
-                    <div style={{marginTop: '4%'}}>
+        return dataFormatada
+    };
+    render() {
+        return (
+            <div>
 
-                        <Button
-                            inverted
-                            circular
-                            icon
-                            labelPosition='left'
-                            color='orange'
-                            floated='right'
-                        >
-                            <Icon name='clipboard outline' />
-                            <Link to={'/form-cliente'}>Novo</Link>
-                        </Button>
-                        <br/><br/><br/>
-                      
-                      <Table color='orange' sortable celled>
+                <div style={{ marginTop: '3%' }}>
 
-                          <Table.Header>
-                              <Table.Row>
-                                  <Table.HeaderCell>Nome</Table.HeaderCell>
-                                  <Table.HeaderCell>CPF</Table.HeaderCell>
-                                  <Table.HeaderCell>Data de Nascimento</Table.HeaderCell>
-                                  <Table.HeaderCell>Fone Celular</Table.HeaderCell>
-                                  <Table.HeaderCell>Fone Fixo</Table.HeaderCell>
-                                  <Table.HeaderCell textAlign='center' width={2}>Ações</Table.HeaderCell>
-                              </Table.Row>
-                          </Table.Header>
-                     
-                          <Table.Body>
+                    <Container textAlign='justified' >
 
-                              { this.state.listaClientes.map(cliente => (
+                        <h2> Cliente </h2>
 
-                                  <Table.Row>
-                                      <Table.Cell>{cliente.nome}</Table.Cell>
-                                      <Table.Cell>{cliente.cpf}</Table.Cell>
-                                      <Table.Cell>{this.formatarData(cliente.dataNascimento)}</Table.Cell>
-                                      <Table.Cell>{cliente.foneCelular}</Table.Cell>
-                                      <Table.Cell>{cliente.foneFixo}</Table.Cell>
-                                      <Table.Cell textAlign='center'>
-                                         
-                                          <Button
-                                              inverted
-                                              circular
-                                              icon='edit'
-                                              color='blue'
-                                              itle='Clique aqui para editar os dados deste cliente' /> &nbsp;
- <Button
-                                                   inverted
-                                                   circular
-                                                   icon='trash'
-                                                   color='red'
-                                                   title='Clique aqui para remover este cliente' />
+                        <Divider />
 
-                                           </Table.Cell>
-                                       </Table.Row>
-                                   ))}
+                        <div style={{ marginTop: '4%' }}>
 
-                               </Table.Body>
-                           </Table>
-                       </div>
-                   </Container>
-               </div>
-           </div>
-       )
-   }
+                            <Button
+                                inverted
+                                circular
+                                icon
+                                labelPosition='left'
+                                color='orange'
+                                floated='right'
+                            >
+                                <Icon name='clipboard outline' />
+                                <Link to={'/form-cliente'}>Novo</Link>
+                            </Button>
+                            <br /><br /><br />
+
+                            <Table color='orange' sortable celled>
+
+                                <Table.Header>
+                                    <Table.Row>
+                                        <Table.HeaderCell>Nome</Table.HeaderCell>
+                                        <Table.HeaderCell>CPF</Table.HeaderCell>
+                                        <Table.HeaderCell>Data de Nascimento</Table.HeaderCell>
+                                        <Table.HeaderCell>Fone Celular</Table.HeaderCell>
+                                        <Table.HeaderCell>Fone Fixo</Table.HeaderCell>
+                                        <Table.HeaderCell textAlign='center' width={2}>Ações</Table.HeaderCell>
+                                    </Table.Row>
+                                </Table.Header>
+
+                                <Table.Body>
+
+                                    {this.state.listaClientes.map(cliente => (
+
+                                        <Table.Row>
+                                            <Table.Cell>{cliente.nome}</Table.Cell>
+                                            <Table.Cell>{cliente.cpf}</Table.Cell>
+                                            <Table.Cell>{this.formatarData(cliente.dataNascimento)}</Table.Cell>
+                                            <Table.Cell>{cliente.foneCelular}</Table.Cell>
+                                            <Table.Cell>{cliente.foneFixo}</Table.Cell>
+                                            <Table.Cell textAlign='center'>
+
+                                                <Button
+                                                    inverted
+                                                    circular
+                                                    icon='edit'
+                                                    color='blue'
+                                                    itle='Clique aqui para editar os dados deste cliente' /> &nbsp;
+                                                <Button
+                                                    inverted
+                                                    circular
+                                                    icon='trash'
+                                                    color='red'
+                                                    title='Clique aqui para remover este cliente' />
+
+                                                <Button
+                                                        inverted
+                                                        circular
+                                                        color='green'
+                                                        title='Clique aqui para editar os dados deste cliente'
+                                                        icon>
+                                                            <Link to="/form-cliente" state={{id: cliente.id}} style={{color: 'green'}}> <Icon name='edit' /> </Link>
+                                                </Button> &nbsp;
+                                            </Table.Cell>
+                                        </Table.Row>
+                                    ))}
+
+                                </Table.Body>
+                            </Table>
+                        </div>
+                    </Container>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default ListCliente;
